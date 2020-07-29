292c44ea06d245aba542de63df3dc4f529cfc114
vit9 <vit91112@gmail.com>
2020-07-29 17:08:50 +0300

    show messages, show messages

--- src/pages/mainScreen/OverviewPartSkeleton.tsx
+++ src/pages/mainScreen/OverviewPartSkeleton.tsx
@@ -3,7 +3,7 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import { Typography, Grid } from '@material-ui/core';
 
 export default () => (
-    <Grid item xs={4} >
+    <Grid item xs={3} >
         {[1, 2, 3, 4, 5].map((element) => <Typography component="div" key={element} variant={'h1'} >
             <Skeleton />
         </Typography>)}
--- src/pages/mainScreen/conversationsPages/UserConversationHistoryPage.tsx
+++ src/pages/mainScreen/conversationsPages/UserConversationHistoryPage.tsx
@@ -1,24 +1,34 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Grid } from '@material-ui/core';
-import { conversationUserHistoryActionRequest } from '../../redux/pages/conversations/constants/actionConstants';
-import { RootState } from '../../redux/reducer';
+import { conversationUserHistoryActionRequest } from '../../../redux/pages/conversations/constants/actionConstants';
+import { RootState } from '../../../redux/reducer';
+import getCorrectDate from '../../../common/getCorrectDateFormat';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  messagesDiv: {
+    display: 'flex',
+    justify-content: 'space-evenly'
+  },
+}));
+
+
 
 export default function UserConversationHistoryPage() {
   const dispatch = useDispatch();
+  const classes = useStyles()
   const messageHistory = useSelector(({ userConversationHistoryReducer }: RootState) => userConversationHistoryReducer.userHistoryConversation.success.data);
-  console.log(messageHistory);
   useEffect(() => {
     dispatch(conversationUserHistoryActionRequest(1));
   }, [dispatch]);
 
   return (
-       <Grid item xs={7}>
+       <Grid item xs={6}>
          {messageHistory.map(({ fkSenderId, message, sendDate }) => (
-             <div>
+             <div className={classes.messagesDiv}>
                <p>{message}</p>
                <p>{fkSenderId}</p>
-               <p>{sendDate}</p>
+               <p>{getCorrectDate(new Date(sendDate))}</p>
              </div>
          ))}
        </Grid>

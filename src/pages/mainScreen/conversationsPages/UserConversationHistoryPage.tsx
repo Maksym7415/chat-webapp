ecc1a03fbcacf785dfb7de953dcd474b46c7d455
vit9 <vit91112@gmail.com>
2020-07-29 17:10:51 +0300

    fix style

--- src/pages/mainScreen/conversationsPages/UserConversationHistoryPage.tsx
+++ src/pages/mainScreen/conversationsPages/UserConversationHistoryPage.tsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { conversationUserHistoryActionRequest } from '../../../redux/pages/conversations/constants/actionConstants';
 import { RootState } from '../../../redux/reducer';
 import getCorrectDate from '../../../common/getCorrectDateFormat';
-import { makeStyles } from '@material-ui/core/styles';
+
 const useStyles = makeStyles((theme) => ({
   messagesDiv: {
     display: 'flex',
-    justify-content: 'space-evenly'
+    justifyContent: 'space-evenly',
   },
 }));
 
-
-
 export default function UserConversationHistoryPage() {
   const dispatch = useDispatch();
-  const classes = useStyles()
+  const classes = useStyles();
   const messageHistory = useSelector(({ userConversationHistoryReducer }: RootState) => userConversationHistoryReducer.userHistoryConversation.success.data);
   useEffect(() => {
     dispatch(conversationUserHistoryActionRequest(1));

76a29ce4b6fd6df517424ff1313d97baa6e88b91
vit9 <vit91112@gmail.com>
2020-08-03 21:08:51 +0300

    improve routing - did it more clear and readeble

---
+++ src/components/PrivatePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Route, Redirect,
+} from 'react-router-dom';
+import AppBarWrapper from './appBar/AppBarWrapper';
+import setConfig from '../routing/config/setConfig';
+
+export default function PrivatePage({
+  Component, roles, isPrivate, token, ...rest
+}) {
+  return (
+            <Route
+                {...rest}
+                render={(props) => {
+                  if (isPrivate) {
+                    if (token) {
+                      if (setConfig(roles, ['superadmin'], false).includes(true)) {
+                        return (
+                                <AppBarWrapper>
+                                    <Component {...props}/>
+                                </AppBarWrapper>
+                        );
+                      }
+                      return <Redirect to='/'/>;
+                    }
+                    return <Redirect to='/signIn'/>;
+                  }
+                  return !token ? <Component {...props}/> : <Redirect to='/'/>;
+                }}
+            />
+  );
+}

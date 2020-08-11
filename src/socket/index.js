a2021f325aadbe295ffc94fa2a75bcc1141d70f7
Maksym7415 <popovmaksim7415@gmail.com>
2020-08-11 15:01:22 +0300

    added host settings for remote connection

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
-export default io('http://localhost:8081');
+// export default io('http://localhost:8081');
 
 // socket for remote connection
 
-// export default io('http://10.4.30.172:8081');
+export default io('http://10.4.30.172:8081');

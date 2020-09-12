f477da39fd1e78d190b19e1fb618ead84d8f8838
Maksym7415 <popovmaksim7415@gmail.com>
2020-09-12 13:47:49 +0300

    fixed bug in check isAvatar function

--- src/pages/mainScreen/helpers/usereHistoryConversations.ts
+++ src/pages/mainScreen/helpers/usereHistoryConversations.ts
@@ -1,6 +1,6 @@
 import { Messages } from '../../../redux/conversations/constants/interfaces';
 
 export function checkIsShowAvatar(array: Array<Messages>, fkSenderId: number, userId: number, index: number) {
-  if (array[index + 1].fkSenderId === userId) return true;
+  if (array[index + 1] && array[index + 1].fkSenderId === userId) return true;
   return false;
 }

e449b5e0379fa4c6e241b4b1410628a7ce74b076
Maksym7415 <popovmaksim7415@gmail.com>
2020-09-26 15:36:42 +0300

    added env variable to socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,7 @@
 import io from 'socket.io-client';
 
-const socketUrl = 'https://stun-server.hopto.org';
+// const socketUrl = 'https://stun-server.hopto.org';
+
+const socketUrl = process.NODE_ENV === 'production' ? process.env.SOCKET_URL_PROD : process.env.SOCKET_URL_DEV;
 
 export default io(socketUrl, { path: '/socket', transports: ['websocket', 'polling'] });

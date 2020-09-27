9fa654df8999f8d8c206d7bc506b80b5f48316be
Maksym7415 <popovmaksim7415@gmail.com>
2020-09-27 14:50:50 +0300

    added check success status in auth

--- src/pages/mainScreen/index.tsx
+++ src/pages/mainScreen/index.tsx
@@ -106,7 +106,8 @@ export default function BasicTextFields({ history }: RouteComponentProps) {
 
   useEffect(() => {
     socket.on(`userIdNewChat${userId}`, (message: Messages, conversationId: number) => {
-      dispatch(getUserConversationsActionRequest());
+      console.log(message, conversationId);
+      // dispatch(getUserConversationsActionRequest());
       dispatch(getConversationIdAction(conversationId, 'Chat'));
       // dispatch(conversationAddNewMessage(message, conversationId));
     });

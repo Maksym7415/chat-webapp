6f630e676253944800b6dd0ce54362e10f0a9a5a
Maksym7415 <popovmaksim7415@gmail.com>
2022-08-10 15:11:30 +0300

    renamed isEditing to isEdit

--- src/pages/mainScreen/components/conversationsPages/components/addFilesComponent.tsx
+++ src/pages/mainScreen/components/conversationsPages/components/addFilesComponent.tsx
@@ -43,7 +43,7 @@ export default function AddFiles({
           fkSenderId: userId,
           sendDate: fullDate(new Date()),
           messageType: 'File',
-          isEditing: false,
+          isEdit: false,
         },
       }, (id: number) => {
         if (id) handleEmitFile(id, fullDate(new Date()));
